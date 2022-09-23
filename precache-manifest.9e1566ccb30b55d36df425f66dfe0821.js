3f099b908f689803faefe48673ca5086b76ad59a
Your Name~ <you@example.com>
2022-09-23 12:55:01 +0300

    Updates

---
+++ precache-manifest.9e1566ccb30b55d36df425f66dfe0821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1104f2d1fa15c943e059179e2fe4c5",
+    "url": "/react-tourss/index.html"
+  },
+  {
+    "revision": "ed083b27a28b04ebaeac",
+    "url": "/react-tourss/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tourss/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tourss/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed083b27a28b04ebaeac",
+    "url": "/react-tourss/static/js/main.4a8dac09.chunk.js"
+  },
+  {
+    "revision": "717648c1a76b2d52ebe0",
+    "url": "/react-tourss/static/js/runtime-main.7e76ac8a.js"
+  }
+]);
